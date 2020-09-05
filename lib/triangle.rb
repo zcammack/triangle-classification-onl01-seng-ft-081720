89c71450faf08dfcd62e151c5852a3823e52ebcd
Zackary Cammack <zack.cammack@gmail.com>
2020-09-05 16:39:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
-  # write code here
+
+  attr_accessor :lengthone, :lengthtwo, :lengththree
+
+  def initialize(lengthone, lengthtwo, lengththree)
+    @lengthone = lengthone
+    @lengthtwo = lengthtwo
+    @lengththree = lengththree
+    @lengths = [@lengthone, @lengthtwo, @lengththree]
+  end
+
+  def valid?
+    (@lengthone + @lengthtwo > @lengththree) &&
+    (@lengthtwo + @lengththree > @lengthone) &&
+    (@lengthone + @lengththree > @lengthtwo)
+  end
+
+  def negative?
+    @lengths.each do |length|
+      if(length <= 0)
+        return true
+      end
+    end
+  end
+
+  def kind
+    if (negative? == true || valid? == false)
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif (@lengthone == @lengthtwo) &&
+          (@lengthtwo == @lengththree)
+            :equilateral
+    elsif (@lengthone == @lengthtwo) ||
+          (@lengthtwo == @lengththree) ||
+          (@lengthone == @lengththree)
+            :isosceles
+    elsif (@lengthone != @lengthtwo) &&
+          (@lengthtwo != @lengththree) &&
+          (@lengthone != @lengththree)
+            :scalene
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "This is not a triangle!"
+    end
+  end
 end
